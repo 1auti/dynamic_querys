59d1274292e60584ed0a9a88b27c4678d6de6af2
Lautaro Julian Cenizo <118015787+1auti@users.noreply.github.com>
2025-09-08 13:26:09 -0300

    Update set_databases.sh

--- set_databases.sh
+++ set_databases.sh
@@ -2,28 +2,6 @@
 # chmod +x set-databases.sh para darle permisos
 # source ./set-databases.sh
 
-export PBA_URL=jdbc:postgresql://192.168.50.122:5432/pba
-export PBA_USERNAME=usuario_pba
-export PBA_PASSWORD=UlbEXeTpiVKX#
-
-export MDA_URL=jdbc:postgresql://192.168.50.122:5432/lapampa
-export MDA_USERNAME=usuario_lapampa
-export MDA_PASSWORD=nv1VLeexA0ZS#
-
-export SANTA_ROSA_URL=jdbc:postgresql://192.168.50.122:5432/avellaneda
-export SANTA_ROSA_USERNAME=usuario_avellaneda
-export SANTA_ROSA_PASSWORD=ty0y5UQhFCug#
-
-export CHACO_URL=jdbc:postgresql://192.168.50.122:5432/chaco
-export CHACO_USERNAME=usuario_chaco
-export CHACO_PASSWORD=iBqIyh5PPRH7#
-
-export ENTRE_RIOS_URL=jdbc:postgresql://192.168.50.122:5432/entrerios
-export ENTRE_RIOS_USERNAME=usuario_entrerios
-export ENTRE_RIOS_PASSWORD=Z4V7KwE7fByw#
-
-export FORMOSA_URL=jdbc:postgresql://192.168.50.122:5432/formosa
-export FORMOSA_USERNAME=usuario_formosa
-export FORMOSA_PASSWORD=bA5Z2Mc48m#
+Aca irian los exports para configurar las variables de entorno
 
 echo "Variables de entorno de las 6 bases cargadas ✅"
